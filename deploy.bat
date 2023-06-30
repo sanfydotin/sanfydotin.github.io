a7780f0d8abaa965373f1bc133791212fe0cd807
Sanjib <138183524+sanfydotin@users.noreply.github.com>
2023-06-30 10:24:54 +0530

    Update title

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 docker-compose -f docker-compose-win.yml up
 
-xcopy _site/ ../sanfydotin.github.io
+xcopy _site ../sanfydotin.github.io /e /i /h
 
 
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230630.101552';
+const cacheName = 'chirpy-20230630.102342';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {

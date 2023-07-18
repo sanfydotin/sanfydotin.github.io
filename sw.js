2ef1d973f9c1bcfe442829c12f786cd58ab78068
Sanfy.In <sanfydotin@gmail.com>
2023-07-18 23:22:16 +0530

    Updated DSA Pattern

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230630.102342';
+const cacheName = 'chirpy-20230718.230727';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
